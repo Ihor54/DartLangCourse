042b867f59b233d0eefa36220345bf49f656c29f
ihor_debelyi <s15293@pjwstk.edu.pl>
2021-08-24 13:17:25 +0200

    finished game rock/paper/scissors

--- rock_paper_scissors/rock_paper_scissors.dart
+++ rock_paper_scissors/rock_paper_scissors.dart
@@ -1,7 +1,37 @@
 import 'dart:io';
 
+import 'dart:math';
+
+enum Move { rock, paper, scissors }
+
 void main() {
-  stdout.write('Rock, paper, scissors? (r/p/s) ');
-  final input = stdin.readLineSync();
-  print(input);
+  final moveDict = {'r': Move.rock, 'p': Move.paper, 's': Move.scissors};
+  final rng = Random();
+
+  while (true) {
+    stdout.write('Rock, paper, scissors? (r/p/s) ');
+    final input = stdin.readLineSync();
+    if (moveDict.containsKey(input)) {
+      final playerMove = moveDict[input];
+      final random = rng.nextInt(3);
+      final aiMove = Move.values[random];
+
+      print('You played: $playerMove');
+      print('AI played: $aiMove');
+
+      if (playerMove == aiMove) {
+        print("It's a draw");
+      } else if (playerMove == Move.scissors && aiMove == Move.paper ||
+          playerMove == Move.paper && aiMove == Move.rock ||
+          playerMove == Move.rock && aiMove == Move.scissors) {
+        print('You win');
+      } else {
+        print('You lose');
+      }
+    } else if (input == 'q') {
+      break;
+    } else {
+      print('Invalid input');
+    }
+  }
 }
